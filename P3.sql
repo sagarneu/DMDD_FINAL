68f5cdd75b9e7928cc8512943838076e93ccd2df
sagarneu <sabbanahallinarend.s@northeastern.edu>
2022-11-20 09:27:27 -0500

    created sql

---
+++ P3.sql
@@ -0,0 +1,16 @@
+SET SERVEROUTPUT on;
+
+DECLARE 
+    user_table_count number;
+    user_group_table_count number;
+    groups_table_count number;
+    events_table_count number;
+    event_tags_table_count number;
+    tags_table_count number;
+    registrations_table_count number;
+    payments_table_count number;
+    location_table_count number;
+    city_table_count number;
+    state_table_count number;
+    country_table_count number;
+    

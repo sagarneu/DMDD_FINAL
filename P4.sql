ad1279e56aaa83e738b0e691f02a38c660f15c15
sagarneu <sabbanahallinarend.s@northeastern.edu>
2022-12-01 00:38:47 -0500

    event

--- P4.sql
+++ P4.sql
@@ -10,7 +10,7 @@ FROM
 where 
   USERNAME = 'MAST112';
 IF(nUser > 0) THEN dbms_output.put_line('USER ALREADY EXISTS');
-ELSE EXECUTE IMMEDIATE 'CREATE USER MAST112 IDENTIFIED BY "Admin_123_property_mgnt"';
+ELSE EXECUTE IMMEDIATE 'CREATE USER MAST112 IDENTIFIED BY "Admin_123_event_mgnt"';
 END IF;
 EXCEPTION WHEN OTHERS THEN dbms_output.put_line(
   dbms_utility.format_error_backtrace
